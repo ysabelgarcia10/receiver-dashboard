82ba561dabff9a179b302afce104d7d9be342603
Ysabel <ysabel.garcia@live.com>
2021-12-15 13:31:38 -0700

    json fetch

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 import "antd/dist/antd.css";
 import OrigExcelPage from "./components/origExcel";
@@ -6,6 +7,27 @@ import ProgExcelPage from "./components/progExcel";
 import Dashboard from "./components/dashboard";
 
 function App() {
+  async function fetchOrigData() {
+    try {
+      const result = await axios.get("http://localhost:8080/api/data/original")
+      console.log("fetching results from original layout...", result.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  async function fetchProgData() {
+    try {
+      const result = await axios.get("http://localhost:8080/api/data/progress")
+      console.log("fetching results from progress layout...", result.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  fetchOrigData();
+  fetchProgData();
+
   return (
     <div className="App">
       <h1>Receiver Layout Progress</h1>
