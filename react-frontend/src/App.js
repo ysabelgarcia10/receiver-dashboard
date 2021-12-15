1552897ad6bcb74bb11b0f93ed0e4187ab8d127c
Ysabel <ysabel.garcia@live.com>
2021-12-14 17:40:19 -0700

    first commit

---
+++ react-frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css';
+import "antd/dist/antd.css";
+import OrigExcelPage from "./components/origExcel";
+import ProgExcelPage from "./components/progExcel";
+import Dashboard from "./components/dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Receiver Layout Progress</h1>
+      <div className="content">
+        <div className="tables">
+          <OrigExcelPage/>
+          <ProgExcelPage/>
+        </div>
+        <Dashboard/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
